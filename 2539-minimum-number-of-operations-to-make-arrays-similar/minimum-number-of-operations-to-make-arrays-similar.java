25d12232be318d51a71266543f4c992521d80fc2
Dev Bhola <devbhola21@gmail.com>
2025-08-02 17:42:02 +0530

    Time: 48 ms (51.28%) | Memory: 60.4 MB (58.97%) - LeetSync

--- 2539-minimum-number-of-operations-to-make-arrays-similar/minimum-number-of-operations-to-make-arrays-similar.java
+++ 2539-minimum-number-of-operations-to-make-arrays-similar/minimum-number-of-operations-to-make-arrays-similar.java
@@ -1,5 +1,7 @@
 class Solution {
     public long makeSimilar(int[] arr, int[] target) {
+        Arrays.sort(arr);
+        Arrays.sort(target);
         List<Integer>e1=new ArrayList<>();
         List<Integer>e2=new ArrayList<>();
         List<Integer>o1=new ArrayList<>();
@@ -10,10 +12,6 @@ class Solution {
             if(target[i]%2==0) e2.add(target[i]);
             else o2.add(target[i]);
         }
-        Collections.sort(e1);
-        Collections.sort(o1);
-        Collections.sort(e2);
-        Collections.sort(o2);
         long ans=0;
         for(int i=0;i<e1.size();i++){
             if(e1.get(i)>e2.get(i)){
