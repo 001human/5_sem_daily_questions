8ab17e02c95d904467242e1e0b9754bf29f26b24
Dev Bhola <devbhola21@gmail.com>
2025-08-11 19:28:41 +0530

    Time: 2 ms (30.85%) | Memory: 41.2 MB (26.60%) - LeetSync

---
+++ 900-reordered-power-of-2/reordered-power-of-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean reorderedPowerOf2(int n) {
+        if(n==1) return true;
+        char[]sb=String.valueOf(n).toCharArray();
+        Arrays.sort(sb);
+        String s1=new String(sb);
+        for(int i=0;i<31;i++){
+            long t=(long)Math.pow(2,i);
+            char[]sb1=String.valueOf(t).toCharArray();
+            Arrays.sort(sb1);
+            String s2=new String(sb1);
+            if(s1.equals(s2)) return true;
+        }
+        return false;
+    }
+}
