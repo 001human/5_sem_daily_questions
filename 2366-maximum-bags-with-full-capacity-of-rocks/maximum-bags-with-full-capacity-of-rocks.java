86dc0abd3c2efbcdb855ea01b6431baf76f6ad58
Dev Bhola <devbhola21@gmail.com>
2025-08-20 19:41:45 +0530

    Time: 15 ms (49.09%) | Memory: 56.6 MB (55.94%) - LeetSync

---
+++ 2366-maximum-bags-with-full-capacity-of-rocks/maximum-bags-with-full-capacity-of-rocks.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maximumBags(int[] c, int[] r, int ex) {
+        int[]dif=new int[c.length];
+        for(int i=0;i<c.length;i++){
+            dif[i]=c[i]-r[i];
+        }
+        Arrays.sort(dif);
+        int ans=0;
+        for(int i=0;i<dif.length;i++){
+            if(dif[i]==0) ans++;
+            else if(dif[i]<=ex){
+                ex-=dif[i];
+                ans++;
+            }
+            else break;
+        }
+        return ans;
+    }
+}
