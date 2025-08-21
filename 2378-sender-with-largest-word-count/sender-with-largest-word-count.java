126ce31e35677104fb19882a5f05ec0b97473a5a
Dev Bhola <devbhola21@gmail.com>
2025-08-21 17:54:41 +0530

    Time: 52 ms (60.53%) | Memory: 55.1 MB (39.91%) - LeetSync

---
+++ 2378-sender-with-largest-word-count/sender-with-largest-word-count.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String largestWordCount(String[] m, String[] s) {
+        int []w=new int[m.length];
+        for(int i=0;i<m.length;i++){
+            String t=m[i];
+            String[]temp=t.split(" ");
+            w[i]=temp.length;
+        }
+        HashMap<String,Integer>map=new HashMap<>();
+        int max=0;
+        for(int i=0;i<w.length;i++){
+            map.put(s[i],map.getOrDefault(s[i],0)+w[i]);
+            max=Math.max(max,map.get(s[i]));
+        }
+        List<String>fin=new ArrayList<>();
+        for(String key:map.keySet()){
+            if(map.get(key)==max) fin.add(key);
+        }
+        PriorityQueue<String>pq=new PriorityQueue<>((a,b)->b.compareTo(a));
+        pq.addAll(fin);
+        return pq.poll();
+    }
+}
