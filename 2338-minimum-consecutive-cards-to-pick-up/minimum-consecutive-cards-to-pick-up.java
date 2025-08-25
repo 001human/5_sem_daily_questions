a6181609a09075ed27b03250383334378c0e8fcf
Dev Bhola <devbhola21@gmail.com>
2025-08-26 00:12:20 +0530

    Time: 94 ms (5.02%) | Memory: 57.9 MB (67.97%) - LeetSync

---
+++ 2338-minimum-consecutive-cards-to-pick-up/minimum-consecutive-cards-to-pick-up.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minimumCardPickup(int[] arr) {
+        HashMap<Integer,Integer>map=new HashMap<>();
+        int ans=Integer.MAX_VALUE;
+        int start=0;
+        int end=0;
+        while(end<arr.length){
+            map.put(arr[end],map.getOrDefault(arr[end],0)+1);
+            while(map.get(arr[end])!=null && map.get(arr[end])==2){
+                ans=Math.min(ans,end-start+1);
+                if(map.get(arr[start])==0) map.remove(arr[start]);
+                map.put(arr[start],map.get(arr[start])-1);
+                start++;
+            }
+            end++;
+        }
+        if(ans==Integer.MAX_VALUE) return -1;
+        return ans;
+    }
+}
