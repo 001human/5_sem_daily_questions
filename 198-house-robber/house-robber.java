3cbccdcb02acaba5ec5b78ef6fe82298c9059004
Dev Bhola <devbhola21@gmail.com>
2025-09-03 19:58:20 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (77.62%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -3,14 +3,38 @@ class Solution {
         int n=arr.length;
         int[]dp=new int[n];
         Arrays.fill(dp,-1);
-        return fun(dp,arr,0);
+        return fun(dp,arr,n-1);
     }
+
     public int fun(int[]dp,int[]arr,int i){
-        if(i>=arr.length) return 0;
+        if(i<0) return 0;
         if(dp[i]!=-1) return dp[i];
-        int rob=arr[i]+fun(dp,arr,i+2);
-        int norob=fun(dp,arr,i+1);
+        int rob=arr[i]+fun(dp,arr,i-2);
+        int norob=fun(dp,arr,i-1);
         dp[i]=Math.max(rob,norob);
         return dp[i];
     }
 }
+
+
+
+
+//recursive  td1
+
+// class Solution {
+//     public int rob(int[] arr) {
+//         int n=arr.length;
+//         int[]dp=new int[n];
+//         Arrays.fill(dp,-1);
+//         return fun(dp,arr,0);
+//     }
+
+//     public int fun(int[]dp,int[]arr,int i){
+//         if(i>=arr.length) return 0;
+//         if(dp[i]!=-1) return dp[i];
+//         int rob=arr[i]+fun(dp,arr,i+2);
+//         int norob=fun(dp,arr,i+1);
+//         dp[i]=Math.max(rob,norob);
+//         return dp[i];
+//     }
+// }
