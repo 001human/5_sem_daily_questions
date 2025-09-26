e1d4adca5f16c49e42ad7ab9759969b9b2489eb3
Dev Bhola <devbhola21@gmail.com>
2025-09-26 14:29:24 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (68.07%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -5,15 +5,18 @@ class Solution {
         while(start<end && arr[start]==arr[start+1]) start++;
         while(end>start && arr[end]==arr[end-1]) end--;
         while(start<=end){
-            int mid=end+(start-end+1)/2;
+            int mid=start+(end-start)/2;
             if(arr[mid]==target) return true;
-            if(arr[mid]>arr[0]){//left sorted
-
-                if(target>=arr[0] && target<arr[mid]) end=mid-1;
+            else if(arr[mid]>=arr[0]){   //left part is sortted
+                if(target>=arr[start] && target<=arr[mid]){
+                    end=mid-1;
+                }
                 else start=mid+1;
             }
-            else{ //left sorted
-                if(target>arr[mid] && target<=arr[arr.length-1]) start=mid+1;
+            else{
+                if(target>arr[mid] && target<=arr[end]){
+                    start=mid+1;
+                }
                 else end=mid-1;
             }
         }
