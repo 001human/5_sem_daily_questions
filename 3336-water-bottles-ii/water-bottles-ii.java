a55a60832c14a4c89b54e713034ea139b2de8c48
Dev Bhola <devbhola21@gmail.com>
2025-10-02 07:37:55 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (57.42%) - LeetSync

--- 3336-water-bottles-ii/water-bottles-ii.java
+++ 3336-water-bottles-ii/water-bottles-ii.java
@@ -1,12 +1,12 @@
 class Solution {
     public int maxBottlesDrunk(int b, int ex) {
         int ans=b;
-        int empty=b;
+        int empty=b; //initially b empty bottles after drinking b bottles
         while(empty>=ex){
             empty=empty-ex;
-            ans++;
-            empty++;
-            ex++;
+            ans++;//new bottle is added
+            empty++;//new bottle is finished so it becomes empty
+            ex++;//icresing numex ny 1
         }
         return ans;
     }
