6ad39896ffde2b8956921c1aa5fa304b9e347d98
Dev Bhola <devbhola21@gmail.com>
2025-10-03 11:03:37 +0530

    Time: 2 ms (84.50%) | Memory: 44.6 MB (64.69%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -2,18 +2,19 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] arr, int t) {
         List<List<Integer>>ans=new ArrayList<>();
         List<Integer>temp=new ArrayList<>();
-        fun(arr,t,0,temp,ans);
+        fun(ans,temp,arr,t,0);
         return ans;
     }
-    public void fun(int[]arr,int t,int i,List<Integer>temp,List<List<Integer>>ans){
+
+    public void fun(List<List<Integer>>ans,List<Integer>temp,int[]arr,int t,int i){
         if(t==0){
             ans.add(new ArrayList(temp));
             return;
         }
         if(i>=arr.length || t<0) return;
         temp.add(arr[i]);
-        fun(arr,t-arr[i],i,temp,ans);
+        fun(ans,temp,arr,t-arr[i],i);
         temp.removeLast();
-        fun(arr,t,i+1,temp,ans);
+        fun(ans,temp,arr,t,i+1);
     }
 }
