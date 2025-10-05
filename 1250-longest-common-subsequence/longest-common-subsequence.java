ae1ac5e54e26fd1c6c062f053ebbbc4e2f8eeead
Dev Bhola <devbhola21@gmail.com>
2025-10-05 13:15:39 +0530

    Time: 72 ms (5.24%) | Memory: 51.4 MB (14.58%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,45 +1,25 @@
 class Solution {
-    public int longestCommonSubsequence(String s1, String s2) {
-        int n=s1.length();
-        int m=s2.length();
-        int[][]dp=new int[n+1][m+1];
-        for(int i=1;i<n+1;i++){
-            for(int j=1;j<m+1;j++){
-                if(s1.charAt(i-1)==s2.charAt(j-1)) dp[i][j]=1+dp[i-1][j-1];
-                else {
-                    int left=dp[i][j-1];
-                    int up=dp[i-1][j];
-                    dp[i][j]=Math.max(left,up);
-                }
-            }
+    public int longestCommonSubsequence(String t1, String t2) {
+        int[][]dp=new int[t1.length()][t2.length()];
+        for(int[]a:dp){
+            Arrays.fill(a,-1);
         }
-        return dp[n][m];
+        return fun(t1,t2,0,0,dp);
+    }
+
+    public int fun(String t1, String t2,int i, int j,int[][]dp){
+        if(i>=t1.length() || j>=t2.length() || t1.equals("") || t2.equals("")) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int ans=0;
+        if(t1.charAt(i)==t2.charAt(j)){
+            ans=1+fun(t1,t2,i+1,j+1,dp);
+        }
+        else {
+            int f=fun(t1,t2,i+1,j,dp);
+            int l=fun(t1,t2,i,j+1,dp);
+            ans=Math.max(f,l);
+        }
+        return dp[i][j]=ans;
     }
 }
 
-
-//top bottom
-
-// class Solution {
-//     public int longestCommonSubsequence(String s1, String s2) {
-//         int[][]dp=new int[s1.length()][s2.length()];
-//         for(int[]a:dp){
-//             Arrays.fill(a,-1);
-//         }
-//         return fun(s1,s2,0,0,dp);
-//     }
-
-//     public int fun(String s1, String s2,int i,int j,int[][]dp){
-//         if(i>=s1.length() || j>=s2.length()) return 0;
-//         if(s1.equals("") || s2.equals("")) return 0;
-//         if(dp[i][j]!=-1) return dp[i][j];
-//         int ans=0;
-//         if(s1.charAt(i)==s2.charAt(j)) {
-//             ans = 1+fun(s1,s2,i+1,j+1,dp);
-//         }
-//         else {
-//             ans= Math.max(fun(s1,s2,i,j+1,dp), fun(s1,s2,i+1,j,dp));
-//         }
-//         return dp[i][j]=ans;
-//     }
-// }
