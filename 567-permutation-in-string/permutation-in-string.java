5c77d4003ff2f4641989db47b9a019c6db2262c7
Dev Bhola <devbhola21@gmail.com>
2025-10-14 11:21:11 +0530

    Time: 5 ms (95.29%) | Memory: 42.6 MB (81.27%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,43 +1,36 @@
 class Solution {
-    public boolean checkInclusion(String p, String s) {
-        int[]fre1=new int[26];
-        for(int i=0;i<p.length();i++){
-            fre1[p.charAt(i)-'a']++;
+    public boolean checkInclusion(String s1, String s2) {
+        int[]arr1=new int[26];
+        int[]arr2=new int[26];
+        if(s1.length()>s2.length()) return false;
+        int check=0;
+        for(int i=0;i<s1.length();i++){
+            arr1[s1.charAt(i)-'a']++;
         }
-        if(p.length()>s.length()) return false;
-        int r=p.length();
-        int l=0;
-        int[]fre2=new int[26];
-        for(int i=0;i<p.length();i++){
-            fre2[s.charAt(i)-'a']++;
+        for(int i=0;i<s1.length();i++){
+            arr2[s2.charAt(i)-'a']++;
         }
 
-        boolean fl=false;
+        boolean fl=true;
         for(int i=0;i<26;i++){
-            if(fre1[i]!=fre2[i]){
-                fl=true;
+            if(arr1[i]!=arr2[i]){
+                fl=false;
                 break;
             }
         }
-
-        if(fl==false) return true;
-        while(r<s.length()){
-            char ch=s.charAt(r);
-            fre2[ch-'a']++;
-            fre2[s.charAt(l)-'a']--;
-            l++;
-            boolean flag=false;
-            for(int i=0;i<26;i++){
-            if(fre1[i]!=fre2[i]){
-                    flag=true;
+        if(fl) return true;
+        for(int i=s1.length();i<s2.length();i++){
+            arr2[s2.charAt(i)-'a']++;
+            arr2[s2.charAt(i-s1.length())-'a']--;
+            boolean flag=true;
+            for(int j=0;j<26;j++){
+                if(arr1[j]!=arr2[j]){
+                    flag=false;
                     break;
                 }
             }
-            if(flag==false) return true;
-            
-            r++;
+            if(flag) return true;
         }
         return false;
     }
 }
-
