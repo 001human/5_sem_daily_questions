70aeb7bf62cf1dc5a4d0f163c8f07190774f0b48
Dev Bhola <devbhola21@gmail.com>
2025-10-14 12:39:10 +0530

    Time: 175 ms (19.31%) | Memory: 125.2 MB (9.41%) - LeetSync

---
+++ 2477-number-of-ways-to-reach-a-position-after-exactly-k-steps/number-of-ways-to-reach-a-position-after-exactly-k-steps.java
@@ -0,0 +1,21 @@
+class Solution {
+    int mod=1000000007;
+    public int numberOfWays(int start, int end, int k) {
+        int[][]dp=new int[10000][k+1];
+        for(int[]ar:dp){
+            Arrays.fill(ar,10001);
+        }
+        return fun(start,end,k,0,dp)%mod;
+    }
+
+    public int fun(int start,int end,int k,int cur,int[][]dp){
+        if(cur==k && start==end){
+            return 1;
+        }
+        if(cur>k) return 0;
+        if(dp[start+1000][cur]!=10001) return dp[start+1000][cur];
+        int a=fun(start+1,end,k,cur+1,dp)%mod;
+        int b=fun(start-1,end,k,cur+1,dp)%mod;
+        return dp[start+1000][cur]=(a+b)%mod;
+    }
+}
