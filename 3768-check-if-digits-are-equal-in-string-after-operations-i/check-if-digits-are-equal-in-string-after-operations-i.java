ebde098b91be2c8c3d7debd05500236e54a5974a
Dev Bhola <devbhola21@gmail.com>
2025-10-23 16:04:10 +0530

    Time: 8 ms (72.03%) | Memory: 44.8 MB (60.13%) - LeetSync

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.java
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.java
@@ -4,7 +4,7 @@ class Solution {
             StringBuilder sb=new StringBuilder();
             for(int i=0;i<s.length()-1;i++){
                 int sum=((int)(s.charAt(i)-'0'+s.charAt(i+1)-'0'))%10;
-                sb.append(""+sum);
+                sb.append(sum);
             }
             s=sb.toString();
         }
