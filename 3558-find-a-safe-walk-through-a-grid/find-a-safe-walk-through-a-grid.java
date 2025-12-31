ffc72158892dbd79ff04c4dfd94fcbe67ca64eae
Dev Bhola <devbhola21@gmail.com>
2025-12-31 23:37:36 +0530

    Time: 150 ms (6.33%) | Memory: 47.2 MB (33.76%) - LeetSync

--- 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.java
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.java
@@ -11,20 +11,19 @@ class Solution {
     }
 
     public boolean fun(List<List<Integer>> arr, int h,int i,int j,int[][]visit,int[][][]dp){
-        if(i>=arr.size() || i<0 || j<0 || j>=arr.get(0).size() || h<1) return false;
+        if(i>=arr.size() || i<0 || j<0 || j>=arr.get(0).size() || visit[i][j]==1) return false;
         if(arr.get(i).get(j)==1) h--;
         if(h<1) return false;
         if(i==arr.size()-1 && j==arr.get(0).size()-1 ){
             return true;
         }
         if(dp[i][j][h]!=-1) return dp[i][j][h]==1;
-        dp[i][j][h]=0;
-        // visit[i][j]=1;
+        visit[i][j]=1;
         boolean up=fun(arr,h,i-1,j,visit,dp);
         boolean down=fun(arr,h,i+1,j,visit,dp);
         boolean right=fun(arr,h,i,j+1,visit,dp);
         boolean left=fun(arr,h,i,j-1,visit,dp);
-        // visit[i][j]=0;
+        visit[i][j]=0;
         boolean r=up || down || right || left;
         dp[i][j][h]= r ? 1:0;
         return r; 
