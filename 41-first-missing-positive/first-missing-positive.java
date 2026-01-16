8e4a8a702cb4eac8222881499193e614e144308b
Dev Bhola <devbhola21@gmail.com>
2026-01-16 11:50:36 +0530

    Time: 2 ms (38.92%) | Memory: 71 MB (93.69%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,40 +1,33 @@
 class Solution {
     public int firstMissingPositive(int[] arr) {
-        //O(n) and O(1)
-        //first swap numbers to original positions
-    //     int n=arr.length;
-    //     for(int i=0;i<n;i++){
-    //         while(arr[i]>0 && arr[i]<=n && arr[i]!=i+1 ){
-    //             swap(i,arr[i]-1,arr);
-    //         }
-    //     }
+        //put the elements in their original positions and then the number which is not
+        //present on that particular index will be the mssing nuber and return it
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==i+1) continue;
+            else {
+                int t1=0;
+                int t2=0;
+                int c=0;
+                while(arr[i]>0 && arr[i]<=arr.length && arr[i]!=i+1){
+                    swap(arr,arr[i]-1,i);
+                    if(c>0 && t1==arr[i]-1 && t2==i) break;
+                    c++;
+                    t1=arr[i]-1;
+                    t2=i;
 
-    //     for(int i=0;i<n;i++){
-    //         if(arr[i]!=i+1) return i+1;
-    //     }
-    //     return n+1;
-    // }
+                }
+            }
+        }
 
-    // public void swap(int i,int j,int[]arr){
-    //     int temp=arr[i];
-    //     arr[i]=arr[j];
-    //     arr[j]=temp;
-    // }
-        int n=arr.length;
-        boolean flag=false;
-        for(int i=0;i<n;i++){
-            if(arr[i]==1) flag=true;
-            if(arr[i]<=0 || arr[i]>n) arr[i]=1;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=i+1) return i+1;
         }
-        if(flag==false) return 1;
-        for(int i=0;i<n;i++){
-            int x=Math.abs(arr[i]);
-            if(arr[x-1]>0) arr[x-1]*=-1;
-        }
-        for(int i=0;i<n;i++){
-            if(arr[i]>0) return i+1;
-        }
-        return n+1;
+        return arr.length+1;
+    }
+
+    public void swap(int[]arr,int i,int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
     }
 }
-
