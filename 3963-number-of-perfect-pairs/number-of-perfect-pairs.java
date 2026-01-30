0767a765b1cda2044fe3427ba412480cdd158df0
Dev Bhola <devbhola21@gmail.com>
2026-01-30 11:33:13 +0530

    Time: 102 ms (16.47%) | Memory: 94 MB (43.71%) - LeetSync

--- 3963-number-of-perfect-pairs/number-of-perfect-pairs.java
+++ 3963-number-of-perfect-pairs/number-of-perfect-pairs.java
@@ -1,18 +1,30 @@
 class Solution {
     public long perfectPairs(int[] arr) {
-        long ans=0;
+        //dif bw max an min cab b max 2 from both dir max<=2*min
         for(int i=0;i<arr.length;i++){
             arr[i]=Math.abs(arr[i]);
         }
         Arrays.sort(arr);
-        int r=1;
-        for(int i=0;i<arr.length;i++){
-            if(r<=i) r=i+1;
-            while(r<arr.length && 1L*arr[r]<=1L*arr[i]*2){
-                r++;
-            }
-            ans+=(r-i-1);
+        long ans=0;
+        for(int i=1;i<arr.length;i++){
+            ans+=pos(i,arr);
         }
         return ans;
     }
-}
+
+    public int pos(int st,int []arr){
+        int end=arr.length-1;
+        int start=st;
+        int ans=-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[st-1]*2>=arr[mid]){
+                ans=mid;
+                start=mid+1;
+            }
+            else end=mid-1;
+        }
+        if(ans==-1) return 0;
+        return ans-st+1;
+    }
+}//1234
