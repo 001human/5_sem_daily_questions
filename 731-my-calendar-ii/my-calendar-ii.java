93bcfc78798d8f144c8f1e39b45b19d88726e1c6
Dev Bhola <devbhola21@gmail.com>
2026-02-03 10:27:19 +0530

    Time: 193 ms (40.39%) | Memory: 47.6 MB (52.69%) - LeetSync

---
+++ 731-my-calendar-ii/my-calendar-ii.java
@@ -0,0 +1,27 @@
+class MyCalendarTwo {
+    List<int[]>arr;
+    public MyCalendarTwo() {
+        arr=new ArrayList<>();
+    }
+    
+    public boolean book(int start, int end) {
+        int[] s = new int[]{start, 1};
+        int[] e = new int[]{end, -1};
+        arr.add(s);
+        arr.add(e);
+        Collections.sort(arr,(a,b)->{
+            if(a[0]==b[0]) return a[1]-b[1];
+            return a[0]-b[0];   
+        });
+        int cur=0;
+        for(int i=0;i<arr.size();i++){
+            cur += arr.get(i)[1];
+            if(cur >2){
+                arr.remove(s); 
+                arr.remove(e);
+                return false;
+            }
+        }
+        return true;
+    }
+}
