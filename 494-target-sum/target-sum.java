2e5bfb8be6177bd5ba053ae08e9de83a87c11f89
Dev Bhola <devbhola21@gmail.com>
2026-03-24 18:09:53 +0530

    Time: 61 ms (44.79%) | Memory: 136.1 MB (5.32%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,18 +1,19 @@
 class Solution {
-    static int ans;
+    static int val=20000;
     public int findTargetSumWays(int[] arr, int t) {
-        ans=0;
-        fun(arr,t,0,0);
-        return ans;
+        int[][]dp=new int[arr.length+1][40001];
+        for(int[]a:dp){
+            Arrays.fill(a,Integer.MIN_VALUE);
+        }
+        return fun(arr,t,0,0,dp);
     }
 
-    public void fun(int[]arr,int t,int sum,int i){
+    public int fun(int[]arr,int t,int sum,int i,int[][]dp){
         if(i==arr.length && sum==t){
-            ans++;
-            return;
+            return 1;
         }
-        if(i>=arr.length) return;
-        fun(arr,t,sum+arr[i],i+1);
-        fun(arr,t,sum-arr[i],i+1);
+        if(i>=arr.length) return 0;
+        if(dp[i][sum+val]!=Integer.MIN_VALUE) return dp[i][sum+val];
+        return dp[i][sum+val] = fun(arr,t,sum+arr[i],i+1,dp) + fun(arr,t,sum-arr[i],i+1,dp);
     }
 }
