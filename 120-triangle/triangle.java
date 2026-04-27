f623b14034e566f7602782aeb8eb7207f0e6657f
Dev Bhola <devbhola21@gmail.com>
2026-04-27 15:16:29 +0530

    Time: 4 ms (47.32%) | Memory: 46.2 MB (27.16%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,20 +1,34 @@
 class Solution {
-    public int minimumTotal(List<List<Integer>> arr) {
-        int[][]dp=new int[arr.size()][arr.size()];
-        for(int[]ar:dp){
-            Arrays.fill(ar,Integer.MAX_VALUE);
+    public int minimumTotal(List<List<Integer>> t) {
+        int r=t.size();
+        int[][]arr=new int[r][r];
+        for(int[]a:arr){
+            Arrays.fill(a,Integer.MAX_VALUE/2);
         }
+
+        for(int i=0;i<t.size();i++){
+            for(int j=0;j<t.get(i).size();j++){
+                arr[i][j]=t.get(i).get(j);
+            }
+        }
+
+        int[][]dp=new int[arr.length][arr.length];
+        for(int[]a:dp){
+            Arrays.fill(a,Integer.MAX_VALUE/2);
+        }
+
         return fun(arr,0,0,dp);
     }
 
-    public int fun(List<List<Integer>> arr,int r,int c,int[][]dp){
-        if(r==arr.size()-1 ){
-            return arr.get(r).get(c);
+    public int fun(int [][]arr,int i,int j,int[][]dp){
+        if(i==arr.length-1 && j<=arr.length-1 && j>=0){
+            return arr[i][j];
         }
-        if(r>=arr.size() || c>=arr.get(r).size() ) return Integer.MAX_VALUE;
-        if(dp[r][c]!=Integer.MAX_VALUE) return dp[r][c];
-        int a=fun(arr,r+1,c,dp);
-        int b=fun(arr,r+1,c+1,dp);
-        return dp[r][c]=Math.min(a,b)+arr.get(r).get(c);
+        if(i<0 || i>=arr.length || j<0 || j>=arr.length || arr[i][j]==Integer.MAX_VALUE/2) return Integer.MAX_VALUE/2;
+        if(dp[i][j]!=Integer.MAX_VALUE/2) return dp[i][j];
+        //2 calls
+        int a = arr[i][j] + fun(arr,i+1,j,dp);
+        int c = arr[i][j] + fun(arr,i+1,j+1,dp); 
+        return dp[i][j]=Math.min(a,c);
     }
 }
