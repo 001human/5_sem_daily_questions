96a2d1fbc4577071d3d143a658934f1981e8ceec
Dev Bhola <devbhola21@gmail.com>
2026-04-28 12:12:40 +0530

    Time: 3 ms (99.58%) | Memory: 87.3 MB (87.44%) - LeetSync

--- 4270-minimum-operations-to-make-array-non-decreasing/minimum-operations-to-make-array-non-decreasing.java
+++ 4270-minimum-operations-to-make-array-non-decreasing/minimum-operations-to-make-array-non-decreasing.java
@@ -1,19 +1,10 @@
 class Solution {
     public long minOperations(int[] arr) {
         long ans=0;
-        int temp1=0;
         for(int i=1;i<arr.length;i++){
-            int temp=0;
             if(arr[i]<arr[i-1]){
-                temp=arr[i-1]-arr[i];
-                if(temp>temp1){
-                    ans+=Math.abs(temp-temp1);
-                    temp1=Math.abs(temp1-temp);
-                    
-                }
-                arr[i]+=temp1;
+                ans+=arr[i-1]-arr[i];
             }
-            else temp1=0;
         }
         return ans;
     }
