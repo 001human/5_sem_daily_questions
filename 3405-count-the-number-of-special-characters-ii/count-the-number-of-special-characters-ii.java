f43b47ac0f9e4adddb04a9ce2c1203809da94fdf
Dev Bhola <devbhola21@gmail.com>
2026-05-27 01:51:48 +0530

    Time: 10 ms (74.79%) | Memory: 48.3 MB (34.30%) - LeetSync

---
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numberOfSpecialChars(String s) {
+        int[]arr1=new int[26];
+        int[]arr2=new int[26];
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch>='A' && ch<='Z'){
+                 arr2[ch-'A']++;
+                // else arr1[(char)(ch+32)-'a']=-1;
+            }
+
+            else{
+                if(arr2[(char)(ch-32)-'A']>0) arr1[ch-'a']=-1;
+                else arr1[ch-'a']++;
+            } 
+        }
+        int ans=0;
+        for(int i=0;i<26;i++){
+            if(arr1[i]>0 && arr2[i]>0) ans++;
+        }
+        return ans;
+    }
+}
