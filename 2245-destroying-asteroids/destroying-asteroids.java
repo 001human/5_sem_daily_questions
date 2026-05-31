bef4fa0cd5b074d128847760ea23fa0a21f1e47a
Dev Bhola <devbhola21@gmail.com>
2026-05-31 11:36:29 +0530

    Time: 26 ms (81.52%) | Memory: 107.3 MB (79.91%) - LeetSync

---
+++ 2245-destroying-asteroids/destroying-asteroids.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean asteroidsDestroyed(int mn, int[] arr) {
+        Arrays.sort(arr);
+        long m=mn;
+        for(int i=0;i<arr.length;i++){
+            if(m>=arr[i]) m+=arr[i];
+            else if(m>arr[arr.length-1]) return true;
+            else return false;
+        }
+        return true;
+    }
+}
