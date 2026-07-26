71559a66529b1deec86f79c0641c5812fdaa987c
Dev Bhola <devbhola21@gmail.com>
2026-07-26 09:47:18 +0530

    Time: 15 ms (80.39%) | Memory: 48.1 MB (57.60%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
@@ -3,8 +3,7 @@ class Solution {
         Arrays.sort(arr);
         int n=arr.length;
         int p1=arr[n-1]*arr[n-2]*arr[n-3];
-        // int p2=arr[0]*arr[1]*arr[2];
-        int p3=arr[0]*arr[1]*arr[n-1];
-        return Math.max(p1,p3);
+        int p2=arr[0]*arr[1]*arr[n-1];
+        return Math.max(p1,p2);
     }
 }
