fdfb3584ef391139107d3478a3ef851282d27dbc
Dev Bhola <devbhola21@gmail.com>
2026-09-10 11:08:00 +0530

    Time: 1 ms (56.32%) | Memory: 45.5 MB (48.87%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.java
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.java
@@ -14,10 +14,11 @@
  * }
  */
 class Solution {
+
     class pair{
         int sum=0;
-        int c=0;
-        int ans=0;
+        int count=0;
+        int ans=0;    
     }
 
     public int averageOfSubtree(TreeNode root) {
@@ -26,18 +27,15 @@ class Solution {
 
     public pair fun(TreeNode root){
         if(root==null) return new pair();
-        pair left=fun(root.left);
         pair right=fun(root.right);
-        pair sdp=new pair();
-        sdp.sum=left.sum+right.sum+root.val;
-        sdp.c=left.c+right.c+1;
-        if(root.val==sdp.sum/sdp.c) {
-            sdp.ans=left.ans+right.ans+1;
-        }
-        else {
-            sdp.ans=left.ans+right.ans;
-        }
-      return sdp;
+        pair left=fun(root.left);
+        pair child=new pair();
+        child.sum=right.sum+left.sum+root.val;
+        child.count=right.count+left.count+1;
+        child.ans=right.ans+left.ans;
+        if(child.sum/child.count==root.val) child.ans++;
+        return child;
 
     }
 }
+
